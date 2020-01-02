f072b7736786ef66aae354230c0ae80a81889cb5
Nudkip <42031840+Nudkip@users.noreply.github.com>
2020-01-02 22:15:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use('/', router);
 
-app.listen(8080, () => {
-    console.log('Server running at port 8080');
+app.listen(8081, () => {
+    console.log('Server running at port 8081');
 });
